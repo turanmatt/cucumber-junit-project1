afa49358c6c18e1a316b9444ffe10d5a973f4501
turanMert <t.meert.k@gmail.com>
2022-11-10 16:07:22 -0500

    jenkinsfile added

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -4,15 +4,17 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
-
     /*
-     Creating a private constructor, so we are closing access to the object of this class
-     from outside of any classes
-     */
+         Creating a private constructor, so we are closing access to the object of this class
+         from outside of any classes
+         */
     private Driver(){}
 
     /*
@@ -28,31 +30,66 @@ public class Driver {
     public static WebDriver getDriver(){
 
         if(driverPool.get() == null){  // if driver/browser was never opened
-
-        String browserType = ConfigurationReader.getProperty("browser");
+            String browserType="";
+            if(System.getProperty("BROWSER")==null){
+                browserType = ConfigurationReader.getProperty("browser");
+            }else {
+                browserType=System.getProperty("BROWSER");
+            }
 
         /*
         Depending on the browserType our switch statement will determine
         to open specific type of browser/driver
          */
-        switch(browserType){
-            case "chrome":
-                WebDriverManager.chromedriver().setup();
-                driverPool.set(new ChromeDriver());
-                driverPool.get().manage().window().maximize();
-                driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-                break;
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                driverPool.set(new FirefoxDriver());
-                driverPool.get().manage().window().maximize();
-                driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-                break;
-        }
+            switch(browserType){
+                case "remote-chrome":
+                    try {
+                        // assign your grid server address
+                        String gridAddress = "54.234.163.94/";
+                        URL url = new URL("http://"+ gridAddress + ":4444/wd/hub");
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("chrome");
+                        driverPool.set(new RemoteWebDriver(url, desiredCapabilities));
+                        //driverPool.set(new RemoteWebDriver(new URL("http://0.0.0.0:4444/wd/hub"),desiredCapabilities));
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case "remote-firefox":
+                    try {
+                        // assign your grid server address
+                        String gridAddress = "174.129.57.20";
+                        URL url = new URL("http://"+ gridAddress + ":4444/wd/hub");
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("firefox");
+                        driverPool.set(new RemoteWebDriver(url, desiredCapabilities));
+                        //driverPool.set(new RemoteWebDriver(new URL("http://0.0.0.0:4444/wd/hub"),desiredCapabilities));
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+                //IP --> 44.201.219.205
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driverPool.set(new ChromeDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driverPool.set(new FirefoxDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+
+            }
         }
 
-    // Same driver instance will be returned every time we call Driver.getDriver() method
-       return driverPool.get();
+        // Same driver instance will be returned every time we call Driver.getDriver() method
+        return driverPool.get();
 
     }
 
@@ -63,5 +100,6 @@ public class Driver {
             driverPool.remove();
         }
     }
+
 
 }
---
+++ src/test/resources/features/us03_modules.feature
@@ -0,0 +1,19 @@
+
+Feature: As a user, I should be access all the main modules of the app.
+
+  Background:
+    Given user on the login page
+
+  Scenario: As a user, I should be access all the main modules of the app.
+    When user  log in with valid credentials
+    Then Verify the user see the following modules:
+      | Dashboard |
+      | Files     |
+      | Photos    |
+      | Activity  |
+      | Talk      |
+      | Mail      |
+      | Contacts  |
+      | Circles   |
+      | Calendar  |
+      | Deck      |
